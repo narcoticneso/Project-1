62c3861bcfe56e13241bd87dfdb020596d3dea76
nathanhamby05 <nathanhamby05@gmail.com>
2026-02-08 16:02:19 -0500

    intersections trimesh

--- SceneObjects/trimesh.cpp
+++ SceneObjects/trimesh.cpp
@@ -5,7 +5,7 @@
 #include <float.h>
 #include <string.h>
 #include "../ui/TraceUI.h"
-extern TraceUI *traceUI;
+
 extern TraceUI *traceUI;
 
 using namespace std;
@@ -75,14 +75,65 @@ bool TrimeshFace::intersect(ray &r, isect &i) const {
   return intersectLocal(r, i);
 }
 
-
-// Intersect ray r with the triangle abc.  If it hits returns true,
-// and put the parameter in t and the barycentric coordinates of the
-// intersection in u (alpha) and v (beta).
+// Intersect ray r with the triangle. If it hits returns true,
+// and fill in the intersection record i.
 bool TrimeshFace::intersectLocal(ray &r, isect &i) const {
-  // YOUR CODE HERE
-  //
-  // FIXME: Add ray-trimesh intersection
+  const double EPS = 1e-9;
+
+  int ia = (*this)[0];
+  int ib = (*this)[1];
+  int ic = (*this)[2];
+
+  const glm::dvec3 &A = parent->vertices[ia];
+  const glm::dvec3 &B = parent->vertices[ib];
+  const glm::dvec3 &C = parent->vertices[ic];
+
+  const glm::dvec3 O = r.getPosition();
+  const glm::dvec3 D = r.getDirection();
+
+  // Möller–Trumbore intersection
+  glm::dvec3 e1 = B - A;
+  glm::dvec3 e2 = C - A;
+
+  glm::dvec3 pvec = glm::cross(D, e2);
+  double det = glm::dot(e1, pvec);
+
+  if (std::fabs(det) < EPS)
+    return false;
+
+  double invDet = 1.0 / det;
+
+  glm::dvec3 tvec = O - A;
+  double u = glm::dot(tvec, pvec) * invDet;
+  if (u < 0.0 || u > 1.0)
+    return false;
+
+  glm::dvec3 qvec = glm::cross(tvec, e1);
+  double v = glm::dot(D, qvec) * invDet;
+  if (v < 0.0 || (u + v) > 1.0)
+    return false;
+
+  double t = glm::dot(e2, qvec) * invDet;
+  if (t <= EPS)
+    return false;
+
+  // Hit!
+  i.setT(t);
+  i.setObject(this->parent);
+
+  double w = 1.0 - u - v;
+
+  // Normal: interpolate vertex normals if present, else face normal
+  glm::dvec3 N;
+  if (parent->vertNorms && !parent->normals.empty()) {
+    const glm::dvec3 &nA = parent->normals[ia];
+    const glm::dvec3 &nB = parent->normals[ib];
+    const glm::dvec3 &nC = parent->normals[ic];
+    N = glm::normalize(w * nA + u * nB + v * nC);
+  } else {
+    N = glm::normalize(glm::cross(e1, e2));
+  }
+  i.setN(N);
 
   /* To determine the color of an intersection, use the following rules:
      - If the parent mesh has non-empty `uvCoords`, barycentrically interpolate
@@ -91,13 +142,35 @@ bool TrimeshFace::intersectLocal(ray &r, isect &i) const {
      - Otherwise, if the parent mesh has non-empty `vertexColors`,
        barycentrically interpolate the colors from the three vertices of the
        face. Create a new material by copying the parent's material, set the
-       diffuse color of this material to the interpolated color, and then 
+       diffuse color of this material to the interpolated color, and then
        assign this material to the intersection.
      - If neither is true, assign the parent's material to the intersection.
   */
 
-  i.setObject(this->parent);
-  return false;
+  if (!parent->uvCoords.empty()) {
+    const glm::dvec2 &uvA = parent->uvCoords[ia];
+    const glm::dvec2 &uvB = parent->uvCoords[ib];
+    const glm::dvec2 &uvC = parent->uvCoords[ic];
+    glm::dvec2 uv = w * uvA + u * uvB + v * uvC;
+    i.setUVCoordinates(uv);
+
+    i.setMaterial(parent->material);
+  }
+  else if (!parent->vertColors.empty()) {
+    const glm::dvec3 &cA = parent->vertColors[ia];
+    const glm::dvec3 &cB = parent->vertColors[ib];
+    const glm::dvec3 &cC = parent->vertColors[ic];
+    glm::dvec3 col = w * cA + u * cB + v * cC;
+
+    Material m = parent->material;
+    m.setDiffuse(col);
+    i.setMaterial(m);
+  }
+  else {
+    i.setMaterial(parent->material);
+  }
+
+  return true;
 }
 
 // Once all the verts and faces are loaded, per vertex normals can be
@@ -123,4 +196,3 @@ void Trimesh::generateNormals() {
 
   vertNorms = true;
 }
-
